0362be0f16a8d621efc1eafbe46db8b581644d1c
William Pannell <lpann5@gmail.com>
2019-11-12 15:20:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,17 @@
 
 def key_for_min_value(name_hash)
   min_key = nil 
+<<<<<<< HEAD
   min_value = nil 
   name_hash.each do |k, v|
     if min_value == nil || v < min_value
       min_value = v 
+=======
+  min_key = nil 
+  name_hash.each do |k, v|
+    if min_key == nil || v < min_key
+      min_key = v 
+>>>>>>> 47cd13f5d9b719f293f85b3a9936647de91aebda
       min_key = k 
     end 
   end 
